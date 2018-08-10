26bbf389173fc447d116aa2697ae0719ccb1802c
Piotr Kwasnik <kwasnik.piotr@gmail.com>
2018-08-11 00:27:34 +0200

    adds argument parsing

--- sony_a6000_utility.py
+++ sony_a6000_utility.py
@@ -5,6 +5,7 @@ import os
 import logging
 import glob
 import shutil
+import argparse
 
 logging.basicConfig(
     format='[%(levelname)s] >>> %(message)s',
@@ -14,12 +15,12 @@ logging.basicConfig(
 )
 
 
-class SonyAlphaFileHandler(object):
+class SonyAlphaFileSystemHandler(object):
     """
     Basic file operations for Sony Alfa camera
     """
 
-    def __init__(self, src_dir, dest_dir):
+    def __init__(self, src_dir, dest_dir=None):
         self.src_dir = src_dir
         self.dest_dir = dest_dir
 
@@ -56,12 +57,35 @@ class SonyAlphaFileHandler(object):
         pass
 
 
+def parse_cmd_line_arguments():
+    """
+    Parse command line arguments passed to the script
+    """
+
+    cmd_line_arg_parser = argparse.ArgumentParser(description='Sony Alpha File System Handler')
+
+    cmd_line_arg_parser.add_argument('--reorganise', action='store_true', required=False,
+                                     help='reorganise sony alpha file structure')
+    cmd_line_arg_parser.add_argument('--src_dir', type=str, required=False,
+                                     help='source directory path')
+    cmd_line_arg_parser.add_argument('--dest_dir', type=str, required=False,
+                                     help='destination directory path')
+
+    return cmd_line_arg_parser.parse_args()
+
+
 if __name__ == '__main__':
 
-    src_dir = '/media/pio/Data&Foto_Slave/SD_2'
-    dest_dir = '/media/pio/Data&Foto_Slave/cool'
+    arguments = parse_cmd_line_arguments()
 
-    alpha = SonyAlphaFileHandler(src_dir, dest_dir)
-    alpha.get_files('JPG')
-    alpha.get_files('ARW')
-    alpha.get_files('MTS')
+    if arguments.reorganise:
+        print('The file structure will be reorganised')
+        # alpha = SonyAlphaFileSystemHandler(arguments.src_dir, arguments.dest_dir)
+        # alpha.get_files('JPG')
+        # alpha.get_files('ARW')
+        # alpha.get_files('MTS')
+
+
+
+
+# --src_dir /media/pio/Data&Foto_Slave/SD_2 --dest_dir /media/pio/Data&Foto_Slave/new_structure
